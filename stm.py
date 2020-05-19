7ce3641db5bb24f9fdc29fc1f644ab687b791bf4
maxium0526 <maxium0526@gmail.com>
2020-05-19 18:43:56 +0800

    module require output_shape now

--- stm.py
+++ stm.py
@@ -2,7 +2,7 @@ from tensorflow.keras.layers import Layer, Conv2D, Dense, Flatten, MaxPooling2D,
 import numpy as np
 from stn import spatial_transformer_network as transformer
 
-def SpatialTransformerModule(image):
+def SpatialTransformerModule(image, output_shape):
 	x = BatchNormalization()(image)
 	x = Conv2D(32,kernel_size=(3,3), padding='same')(x)
 	x = BatchNormalization()(x)
@@ -19,7 +19,7 @@ def SpatialTransformerModule(image):
 	x = Flatten()(x)
 	x = Dense(100, activation='tanh')(x)
 	x = AffineMatrix(name='mat')(x)
-	out = SpatialTransformer(name='stn')([image, x])
+	out = SpatialTransformer(output_shape, name='stn')([image, x])
 
 	return out
 
@@ -39,7 +39,7 @@ class AffineMatrix(Layer):
 		return out
 
 class SpatialTransformer(Layer):
-	def __init__(self, output_shape=None, **kwargs):
+	def __init__(self, output_shape, **kwargs):
 		super(SpatialTransformer, self).__init__(**kwargs)
 		self.transformer_output_shape = output_shape
 
@@ -47,9 +47,6 @@ class SpatialTransformer(Layer):
 		image = tensors[0]
 		matrix = tensors[1]
 
-		if self.transformer_output_shape is None:
-			out = transformer(image, matrix)
-		else:
-			out = transformer(image, matrix, self.transformer_output_shape)
+		out = transformer(image, matrix, self.transformer_output_shape)
 
 		return out
